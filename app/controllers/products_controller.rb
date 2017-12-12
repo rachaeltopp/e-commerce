a919fcf6b857fbfd00f4961de96691a0987b2da8
Rachael Topp <topp@ualberta.ca>
2017-12-12 12:27:08 -0400

    see all products and links to pages

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,12 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
   def new
   end
 
@@ -7,10 +15,6 @@ class ProductsController < ApplicationController
 
     @product.save
     redirect_to @product
-  end
-
-  def show
-    @product = Product.find(params[:id])
   end
 
   private
