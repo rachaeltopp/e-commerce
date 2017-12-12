db7d6c555e19d34d046522e7bf9a4e24b094bf34
Rachael Topp <topp@ualberta.ca>
2017-12-12 12:14:56 -0400

    create and show products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,18 @@ class ProductsController < ApplicationController
   end
 
   def create
-    render plain: params[:article].inspect
+    @product = Product.new(product_params)
+
+    @product.save
+    redirect_to @product
   end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  private
+    def product_params
+      params.require(:product).permit(:title, :text)
+    end
 end
