2fab685171daf695f9b33ae3f462912b5aba084b
Rachael Topp <topp@ualberta.ca>
2017-12-21 15:19:57 -0400

    ability to delete product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -35,6 +35,13 @@ class ProductsController < ApplicationController
     end
   end
 
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+
+    redirect_to products_path
+  end
+
   private
     def product_params
       params.require(:product).permit(:title, :text)
